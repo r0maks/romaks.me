61e044fb50f3ac19dcc2a88575b692bc724a95a5
r0maks <rrmaksimov@gmail.com>
2017-01-29 20:23:45 -0500

    added functionality for pulling images based on album id from flickr

--- js/app.js
+++ js/app.js
@@ -1,25 +1,45 @@
 var app = angular.module('mySite', []);
-app.controller('NavController', function() {
+app.controller('NavController', ['$http', function ($http) {
     var vm = this;
 
-            // functions 
-        vm.checkActive = checkActive;
-        vm.setTab = setTab;
+    // functions 
+    vm.checkActive = checkActive;
+    vm.setTab = setTab;
+    vm.getShowcaseImages = getShowcaseImages;
 
-        function checkActive(tabVal){
-            if(tabVal === vm.currentTab){
-                return 'active';
-            }
-            return null;
+
+    var request = ''
+
+    function checkActive(tabVal) {
+        if (tabVal === vm.currentTab) {
+            return 'active';
         }
+        return null;
+    }
 
-        function setTab(tabVal){
-            vm.currentTab = tabVal;
-        }
+    function setTab(tabVal) {
+        vm.currentTab = tabVal;
+    }
 
-        activate ();
+    function getShowcaseImages() {
 
-        function activate(){
-            vm.currentTab = null;
-        }
-});
+        var showCaseAlbumId = '72157672995329216';
+        return getAlbumImages(showCaseAlbumId);
+    }
+
+
+    function getAlbumImages(photoSetId){
+      return $http.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=a204dbf28ba325b9f717d739309ac663&photoset_id='+photoSetId+'&extras=url_q%2C+url_z%2Curl_l%2Ctags&format=json&nojsoncallback=1 ')
+      .success(function (data) {
+        var rows = data;     
+    });
+    }
+
+    activate();
+
+    function activate() {
+        vm.currentTab = null;
+
+        getShowcaseImages();
+    }
+}]);
