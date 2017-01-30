68d1bb30c87c59d7ebcbb22a4edd707d1bdae047
r0maks <rrmaksimov@gmail.com>
2017-01-29 20:53:16 -0500

    added functionality to get images from specific flickr album and cycle through them

--- js/app.js
+++ js/app.js
@@ -2,13 +2,15 @@ var app = angular.module('mySite', []);
 app.controller('NavController', ['$http', function ($http) {
     var vm = this;
 
+    vm.photos = [];
+    
+    // start on 0 index 
+    vm.currentImageIndex = 0;
     // functions 
     vm.checkActive = checkActive;
     vm.setTab = setTab;
     vm.getShowcaseImages = getShowcaseImages;
-
-
-    var request = ''
+    vm.changeImage = changeImage;
 
     function checkActive(tabVal) {
         if (tabVal === vm.currentTab) {
@@ -17,21 +19,33 @@ app.controller('NavController', ['$http', function ($http) {
         return null;
     }
 
+    // pass in -1 or +1
+    function changeImage(direction){
+
+        vm.currentImageIndex = vm.currentImageIndex + direction;
+        vm.currentImage = vm.photos[vm.currentImageIndex];
+
+        document.getElementById('currentImage').src=vm.currentImage.url_l;
+    }
+
     function setTab(tabVal) {
         vm.currentTab = tabVal;
     }
 
+    //TODO Make a showcase album set
     function getShowcaseImages() {
 
         var showCaseAlbumId = '72157672995329216';
         return getAlbumImages(showCaseAlbumId);
     }
 
-
     function getAlbumImages(photoSetId){
-      return $http.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=a204dbf28ba325b9f717d739309ac663&photoset_id='+photoSetId+'&extras=url_q%2C+url_z%2Curl_l%2Ctags&format=json&nojsoncallback=1 ')
+      return $http.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=a204dbf28ba325b9f717d739309ac663&photoset_id='+photoSetId+'&extras=url_q%2C+url_z%2Curl_l%2Ctags&format=json&nojsoncallback=1')
       .success(function (data) {
-        var rows = data;     
+        if(data && data.photoset && data.photoset.photo.length){
+            vm.photos = data.photoset.photo;     
+            vm.currentImage = vm.photos[0];     
+        }     
     });
     }
 
@@ -42,4 +56,6 @@ app.controller('NavController', ['$http', function ($http) {
 
         getShowcaseImages();
     }
+
+
 }]);
