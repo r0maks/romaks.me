319144f09f07cd65014fd9cfa204a560003e634c
r0maks <rrmaksimov@gmail.com>
2017-08-02 23:18:30 -0400

    making it rain

--- p5/rain/rain.js
+++ p5/rain/rain.js
@@ -3,38 +3,54 @@ var rainDrops = [];
 
 function setup() {
     createCanvas(w, 750);
-
-}
-
-function draw() {
-
-    background(30);
     generateDrops();
 }
 
 function generateDrops(){
     
-    stroke(230);
     // start with 100 drops
-    for(var i=0; i<30; i++) {
-
-        var x1 = random(0, w-1);
-        var x2 = x1+1;
-        var y1 = 1;
-        var y2 = 5;
-        rainDrops.push(line(x1, y1, x2, y2));
-
+    for (var i = 0; i < 2000; i++) {
+        rainDrops[i] = new Drop();
     }
-    rainDrops = [];
 
 }
 
+function draw() {
+
+    background(20);
+    for (var i = 0; i < rainDrops.length; i++) {
+        rainDrops[i].fall();
+        rainDrops[i].show();
+  }
+}
+
 function Drop() {
-  this.x = random(width);
-  this.y = random(-500, -50);
-  this.z = random(0, 20);
-  this.len = map(this.z, 0, 20, 10, 20);
-  this.yspeed = map(this.z, 0, 20, 1, 20);
+ 
+    this.x1 = random(0, w-1);
+    this.x2 = this.x1+2;
+    this.y1 = 1;
+    this.y2 = 2;
+    this.z = random(0, 20);
+    this.yspeed = map(this.z, 0, 20, 1, 20);
+
+    this.show = function(){
+
+        stroke(255, 50, 255);
+        line(this.x1, this.y1, this.x2, this.y2);
+    }
+
+    this.fall = function() {
+        this.y1 = this.y1 + this.yspeed;
+        this.y2 = this.y2 + this.yspeed;
+        var grav = map(this.z, 0, 20, 0, 0.2);
+        this.yspeed = this.yspeed + grav;
+
+
+        if(this.y1 > height){
+            this.y1 = this.y2 = random(-200, -100);
+            this.yspeed = map(this.z, 0, 20, 4, 10);
+        }
+    }
 
 }
 
