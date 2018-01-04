c802b77d515b1b0f9dae7a1781ea01f3e398936b
r0maks <rrmaksimov@gmail.com>
2018-01-03 21:25:58 -0500

    made photo viewer into its own directive

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var app = angular.module('mySite', []);
 app.controller('NavController', ['$http', '$scope', '$window', function ($http, $scope, $window) {
     var vm = this;
 
-    vm.photos = [];
+    vm.portraits = [];
     vm.currentTab = 1;
     // start on 0 index 
     vm.currentImageIndex = 0;
@@ -12,9 +12,7 @@ app.controller('NavController', ['$http', '$scope', '$window', function ($http, 
     vm.checkActive = checkActive;
     vm.setTab = setTab;
     vm.getPortraitImages = getPortraitImages;
-    vm.changeImage = changeImage;
-    vm.setActiveImage = setActiveImage;
-    vm.checkDisable = checkDisable;
+    vm.setActivePortrait = setActivePortrait;
     vm.goLink = goLink;
     vm.imgLoaded = imgLoaded;
 
@@ -25,33 +23,10 @@ app.controller('NavController', ['$http', '$scope', '$window', function ($http, 
         return 'inactive';
     }
 
-    // pass in +1 or -1
-    function checkDisable(direction) {
 
-        if (!vm.photos || !vm.photos.length || vm.photos.length < 1) {
-            return true;
-        }
+    function setActivePortrait(index) {
 
-        var resultIndex = vm.currentImageIndex + direction;
-        var totalLength = vm.photos.length;
-
-        if (resultIndex < 0 || resultIndex > (totalLength - 1)) {
-            return true;
-        }
-        return false;
-    }
-
-    // pass in -1 or +1
-    function changeImage(direction) {
-
-        vm.imageLoading = true;
-        vm.currentImageIndex = vm.currentImageIndex + direction;
-        vm.currentImage = vm.photos[vm.currentImageIndex];
-    }
-
-    function setActiveImage(index) {
-        vm.currentImageIndex = index;
-        vm.currentImage = vm.photos[vm.currentImageIndex];
+        $scope.$broadcast('show-photo', {index: index, photos: vm.portraits});
     }
 
     function setTab(tabVal) {
@@ -84,34 +59,31 @@ app.controller('NavController', ['$http', '$scope', '$window', function ($http, 
     function imgLoaded(image) {
         vm.portraitsLoaded++;
 
-        if (vm.portraitsLoaded === vm.photos.length) {
+        if (vm.portraitsLoaded === vm.portraits.length) {
             vm.allPortraitsLoaded = true;
         }
 
-        vm.portraitPercentage = (vm.portraitsLoaded / vm.photos.length) * 100;
+        vm.portraitPercentage = (vm.portraitsLoaded / vm.portraits.length) * 100;
         vm.portraitPercentage = Math.round(vm.portraitPercentage * 100) / 100
     }
 
     //TODO Make a showcase album set
     function getPortraitImages() {
         var showCaseAlbumId = '72157665034972542';
-        return getAlbumImages(showCaseAlbumId, vm.portraitImagesLoaded);
+        getAlbumImages(showCaseAlbumId)
+        .success(function (data) {
+            if (data && data.photoset && data.photoset.photo.length) {
+                vm.portraits = data.photoset.photo;
+                vm.isLoading = false;
+                if (!vm.portraitImagesLoaded) {
+                    vm.portraitImagesLoaded = true
+                }
+            }
+        });
     }
 
-    function getAlbumImages(photoSetId, flagToSet) {
-        vm.isLoading = true;
-        return $http.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=a204dbf28ba325b9f717d739309ac663&photoset_id=' + photoSetId + '&extras=url_q%2C+url_z%2Curl_l%2Ctags&format=json&nojsoncallback=1')
-            .success(function (data) {
-                if (data && data.photoset && data.photoset.photo.length) {
-                    vm.photos = data.photoset.photo;
-                    //shuffleSet(vm.photos);
-                    vm.currentImage = vm.photos[0];
-                    vm.isLoading = false;
-                    if (flagToSet == null || flagToSet == false) {
-                        flagToSet = true
-                    }
-                }
-            });
+    function getAlbumImages(photoSetId) {
+        return $http.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=a204dbf28ba325b9f717d739309ac663&photoset_id=' + photoSetId + '&extras=url_q%2C+url_z%2Curl_l%2Ctags&format=json&nojsoncallback=1');
     }
 
     // shuffles photoset so they display in a different order every time
@@ -138,29 +110,6 @@ app.directive('imageonload', function () {
     };
 });
 
-app.directive('photoViewer', function () {
-    
-    function PhotoViewerController () {
-        
-    }
-
-    return {
-        restrict: 'EA',
-        scope: {
-            photoSetId: '=',
-        },
-        controllerAs: 'vm',
-        controller: PhotoViewerController,
-        templateUrl: 'photo-viewer.html'
-    };
-});
-
-app.service('flickrService', function() {
-    this.myFunc = function (x) {
-        return x.toString(16);
-    }
-});
-
 $(function () {
     $(document).scroll(function () {
         var $nav = $(".navbar-fixed-top");
@@ -173,3 +122,80 @@ $(document).on('click', '.navbar-collapse.in', function (e) {
         $(this).collapse('hide');
     }
 });
+
+
+(function () {
+    'use strict';
+
+    angular
+        .module('mySite')
+        .directive('photoViewer', PhotoViewerDirective);
+
+    function PhotoViewerDirective() {
+        // Usage:
+        //
+        // Creates:
+        //
+        var directive = {
+            bindToController: true,
+            restrict: 'EA',
+            scope: {
+            },
+            controllerAs: 'vm',
+            controller: PhotoViewerController,
+            templateUrl: 'photo-viewer.html'
+        };
+        return directive;
+
+    }
+    /* @ngInject */
+    function PhotoViewerController($scope) {
+
+        var vm = this;
+        vm.currentImageIndex = 0;
+        vm.currentImage = null;
+        vm.changeImage = changeImage;
+        vm.checkDisable = checkDisable;
+        vm.imageLoading = true;
+        vm.element = angular.element('#imageDialog');
+
+        // pass in +1 or -1
+        function checkDisable(direction) {
+
+            if (!vm.photos || !vm.photos.length || vm.photos.length < 1) {
+                return true;
+            }
+
+            var resultIndex = vm.currentImageIndex + direction;
+            var totalLength = vm.photos.length;
+
+            if (resultIndex < 0 || resultIndex > (totalLength - 1)) {
+                return true;
+            }
+            return false;
+        }
+
+        // pass in -1 or +1
+        function changeImage(direction) {
+
+            vm.imageLoading = true;
+            vm.currentImageIndex = vm.currentImageIndex + direction;
+            vm.currentImage = vm.photos[vm.currentImageIndex];
+        }
+
+        activate();
+
+        function activate() {
+
+            // listen for a set of photos as well as an index at which to start
+            $scope.$on('show-photo', function(event, args) { 
+                vm.currentImageIndex = args.index;
+                vm.photos = args.photos
+                vm.currentImage = vm.photos[vm.currentImageIndex];
+                vm.element.modal('show');
+            });
+
+        }
+
+    }
+})();
