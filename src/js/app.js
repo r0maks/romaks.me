ac4406c08be5cbdd1668eb6c4637ea4f57af60d3
Roman Maksimov <rrmaksimov@gmail.com>
2019-04-09 18:16:02 -0400

    cracked some things

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,23 @@
-'use strict';
-
-/**Optionally add Bootstrap to your project**/
-//import 'bootstrap';
 
 import '../css/style.scss';
+var tab = 1;
+var tabPrefix = 'tab-';
+function changeTab(tabIndex) {
+    hideElement(tabPrefix + tab);
+    tab = tabIndex;
+    console.log(tab);
+    showElement(tabPrefix + tabIndex);
+}
+function showElement(elemId) {
+    let elem = document.getElementById(elemId);
+    if (elem) {
+        elem.style.display = elem.style.displayLast;
+    }
+}
+function hideElement(elemId) {
+    let elem = document.getElementById(elemId);
+    if (elem) {
+        elem.style.displayLast = elem.style.display;
+        elem.style.display = 'none';
+    }
+}
