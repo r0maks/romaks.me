9a7cf6ccb3bbb0bafd1b65004ca5484deade4622
Roman Maksimov <rrmaksimov@gmail.com>
2019-04-09 20:17:20 -0400

    tab system in vanilla js

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,2 @@
 
 import '../css/style.scss';
-var tab = 1;
-var tabPrefix = 'tab-';
-function changeTab(tabIndex) {
-    hideElement(tabPrefix + tab);
-    tab = tabIndex;
-    console.log(tab);
-    showElement(tabPrefix + tabIndex);
-}
-function showElement(elemId) {
-    let elem = document.getElementById(elemId);
-    if (elem) {
-        elem.style.display = elem.style.displayLast;
-    }
-}
-function hideElement(elemId) {
-    let elem = document.getElementById(elemId);
-    if (elem) {
-        elem.style.displayLast = elem.style.display;
-        elem.style.display = 'none';
-    }
-}
